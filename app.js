8f07d833cc44aed443946ea4f7dc68381f4f8be7
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-03 13:36:03 +0700

    Doc Readme

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
